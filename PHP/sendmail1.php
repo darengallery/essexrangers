67b5fb0f5d2efc46e6cbf577cad1d53db7ae53b7
Daren Gallery <darengallery@gmail.com>
2020-07-07 16:08:17 +0100

    added code

--- PHP/sendmail1.php
+++ PHP/sendmail1.php
@@ -20,7 +20,7 @@ $recipient = "daren_gallery@hotmail.com";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank You!" . " -" . "<a href='index.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
+echo "Thank You!" . " -" . "<a href='../index.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
 exit;
 ?>
 
