a9f4520a2ef430049d201e48a7252ac456e3c71c
Daren Gallery <darengallery@gmail.com>
2020-07-07 15:59:49 +0100

    code

--- PHP/sendmail.php
+++ PHP/sendmail.php
@@ -9,8 +9,8 @@ $playersSchoolPostcode = $_POST['playersSchoolPostcode'];
 $playersSchoolYear = $_POST['playersSchoolYear'];
 $playersHomeAddress = $_POST['playersHomeAddress'];
 $playersHomePostcode = $_POST['playersHomePostcode'];
-$contactFirstName = $_POST['contactFirstName'];
-$contactLastName = $_POST['contactLastName'];
+$name = $_POST['name'];
+$lastName = $_POST['lastName'];
 $relationship = $_POST['relationship'];
 $homeAddress = $_POST['homeAddress'];
 $homePostcode = $_POST['homePostcode'];
@@ -21,13 +21,13 @@ $medical = $_POST['medical'];
 $message = $_POST['message'];
 $optradio = $_POST['optradio'];
 
-$formcontent="From: $firstName . $lastName \n Players First Name: $playersFirstName \n PLayers Last Name: $playersLastName \n Players date-of-Birth: $dob \n Players School:  $playersSchool \n Players School Address: $playersSchoolAddress \n Players School postcode: $playersSchoolPostcode \n Players School Year: $playersSchoolYear \n Players Home Address: $playersHomeAddress \n Players Home Postcode: $playersHomePostcode \n First Name: $contactFirstName \n Last Name: $contactLastName \n Relationship to player: $relationship \n Home Address: $homeAddress \n Home Postcode: $homePostcode \n Home Phone: $homePhone \n Mobile Phone: $mobilePhone \n Email Address: $email \n Medical: $medical \n Message: $message \n First Aid: $optradio";
+$formcontent="From: $name . $lastName \n Players First Name: $playersFirstName \n PLayers Last Name: $playersLastName \n Players date-of-Birth: $dob \n Players School:  $playersSchool \n Players School Address: $playersSchoolAddress \n Players School postcode: $playersSchoolPostcode \n Players School Year: $playersSchoolYear \n Players Home Address: $playersHomeAddress \n Players Home Postcode: $playersHomePostcode \n First Name: $name \n Last Name: $lastName \n Relationship to player: $relationship \n Home Address: $homeAddress \n Home Postcode: $homePostcode \n Home Phone: $homePhone \n Mobile Phone: $mobilePhone \n Email Address: $email \n Medical: $medical \n Message: $message \n First Aid: $optradio";
 
 $recipient = "daren_gallery@hotmail.com";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-echo "Thank You!" . " -" . "<a href='index.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
+echo "Thank You!" . " -" . "<a href='../index.html' style='text-decoration:none;color:#ff0099;'> Return Home</a>";
 exit;
 ?>
 
